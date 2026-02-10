1232c1861a10a712e2ac3790e90fc2d258bb0c44
Ankit <ankit@Mac.fritz.box>
2026-02-10 21:20:07 +0100

    frontend structure + tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-green-600 flex items-center justify-center text-white text-5xl font-bold">
+      Tailwind Working 🚀
+    </div>
+  );
+}
+
+export default App;
